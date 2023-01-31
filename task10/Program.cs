42d0b970c8ae7eccd1e37798492aec35118cb6eb
Pavel Danilov <danilov211@mail.ru>
2023-01-31 20:44:14 +0300

    add task10

---
+++ task10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine());
+int secondNumber = (number % 100) / 10;
+//secondNumber = secondNumber / 10;
+Console.WriteLine($"Вы ввели {number}, вторая цифра этого числа {secondNumber}");
